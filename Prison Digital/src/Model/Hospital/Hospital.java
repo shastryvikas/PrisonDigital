87b9a72cfd32d589f173ae2a99533483ecd05a22
Vikas Venkataraman Shastry <shastry.v@northeastern.edu>
2021-12-07 17:06:09 -0500

    FirstCommit - model dev

---
+++ Prison Digital/src/Model/Hospital/Hospital.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Hospital;
+
+import Model.Enterprise;
+
+/**
+ *
+ * @author vikasshastry
+ */
+public class Hospital extends Enterprise{
+    
+}
---
+++ Prison Digital/src/Model/PrisonEcosystem.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Model.CriminalJusticeSystem.CriminalJusticeSystem;
+import Model.FoodCateringService.FoodCateringService;
+import Model.Hospital.Hospital;
+import Model.Prison.Prison;
+
+/**
+ *
+ * @author vikasshastry
+ */
+public class PrisonEcosystem {
+    
+    Prison[] prisons;
+    FoodCateringService[] cateringServices;
+    Hospital[] hospitals;
+    CriminalJusticeSystem[] criminalJusticeSystems;
+    
+    
+    
+}
